f748ffd4d52e6b2ae9928c1a973d7b8b75ed207f
jpablogomez1913 <juangomezramirez527@gmail.com>
2026-02-15 18:15:33 -0500

    Adición factory method

--- Creator(GRASP)/Customers/Customer.cs
+++ Creator(GRASP)/Customers/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using Creator_GRASP_.Orders;
+using Creator_GRASP_.Producto;
 
 namespace Creator_GRASP_.Customers
 {
@@ -6,20 +7,31 @@ namespace Creator_GRASP_.Customers
     public class Customer
     {
         //Los constructores privados evitan la creación descontrolada de objetos
-        private Customer(int id, string email, string name)
+        private Customer()
         {
-            CustomerId = id;
-            Email = email;
-            Name = name;
-                
         }
 
         public int CustomerId { get; private set; }
         public string Email { get; private set; } = string.Empty;
         public string Name { get; private set; } = string.Empty;
 
+        // Se usa un Factory Method para delegar la creacion de customer´s  y evitar la aparicion de new() por todas partes
+        public static Customer Register(string email, string name)
+        {
+
+            return new Customer
+            {
+                CustomerId = 1,
+                Email = email,
+                Name = name,
+            };
+        }
         public Order CreateOrder()
         {
+            //Ya no se puede crear new por todas partes
+            var producto = new Product();
+            var order = new Order();
+
             // Customer solo crea el aggregate Order
             // No conoce ni crea LineItems
             return Order.Create(this);
--- Creator(GRASP)/Products/Product.cs
+++ Creator(GRASP)/Products/Product.cs
@@ -2,10 +2,25 @@
 {
     public class Product
     {
+        private Product()
+        {
+                
+        }
         public int Id { get; private set; }
         public string Name { get; private set; } = string.Empty;
         public decimal Price { get; private set; }
         public string Currency { get; private set; } = string.Empty;
         public int Inventory { get; set; }
+
+        public static Product Register(string name,decimal price,string currency,int inventory) {
+
+            return new Product {
+                Name = name,
+                Price = price,
+                Currency = currency,
+                Inventory = inventory
+            };
+
+        }
     }
 }
